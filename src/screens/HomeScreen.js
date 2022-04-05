93a680a3c2561a19129a3bdab5cee0d2cf452dbc
Lucia Sheridan <nluciasheridan@gmail.com>
2022-04-05 19:05:22 -0300

    Text Demo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,7 +21,10 @@ const HomeScreen = ({navigation}) => {
       title="Go to Color Demo"/>
     <Button 
       onPress={() => navigation.navigate('Square')}
-      title="Go to Square Demo"/>    
+      title="Go to Square Demo"/>
+    <Button 
+      onPress={() => navigation.navigate('Text')}
+      title="Go to Text Demo"/>    
   {/*<TouchableOpacity onPress={() => navigation.navigate('List')}>
     <Text>Go to List Demo</Text>
   </TouchableOpacity> */}
